86c9bd71e34a09d54db1d4ecbacd7f7fc370be89
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-09-16 19:19:12 +0530

    jsx intro

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Maulik Patel"; 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Maulik Patel
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Plans</li>
+      <li>Contact Us</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, alias similique! Earum ex animi autem atque, praesentium repellat nobis sunt?</p>
     </div>
+    </>
   );
 }
 
