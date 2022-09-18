c39665cba7f73fb7dfd5bf4aa121fabbb4b6b090
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-09-18 12:33:58 +0530

    states
    understanding state and handling events in React.

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handleOnChange = (event)=>{
+        console.log("On change");
+        setText(event.target.value);
+    }
+    // Declare a new state variable, which we'll call "count"
+    const [text, setText] = useState('enter your text here');
+    // setText("new text"); // correct way to change state
     return (
+
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to UPPER CASE</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UPPER CASE</button>
         </div>
     )
 }
