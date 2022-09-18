1cd628d4e87754b9a13785edee95acf18332e5a0
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-09-18 13:05:34 +0530

    logic
    logic to count words and characters

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -6,6 +6,11 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLoClick = ()=>{
+        console.log("Lowercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleOnChange = (event)=>{
         console.log("On change");
         setText(event.target.value);
@@ -14,13 +19,22 @@ export default function TextForm(props) {
     const [text, setText] = useState('enter your text here');
     // setText("new text"); // correct way to change state
     return (
-
+        <>
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UPPER CASE</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UPPER CASE</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lower case</button>
         </div>
+        <div className="container my-2">
+            <h2>Your text summery</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes to read this article</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
